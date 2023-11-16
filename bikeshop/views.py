6a82baf63e585a714a40a328ac79869e4e55f806
PreethiBompally <pb54108@uga.edu>
2023-11-16 02:26:36 -0500

    Orders pagination

--- bikeshop/views.py
+++ bikeshop/views.py
@@ -6,6 +6,7 @@ from .models import Customers, Products, Stocks, Orders
 from django.urls import reverse
 from staff.models import Staff,Stores
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def login_view(request):
     if request.method == 'POST':
@@ -99,30 +100,6 @@ def products(request):
     else:
         return render(request, 'products.html', context)
 
-# def stocks(request):
-#     all_stocks = Stocks.objects.all()
-    
-#     if request.method == 'POST':
-#         search_str = request.POST.get('search')
-#         filter_value = request.POST.get('filter')
-#         if(filter_value != '' and search_str != ''):
-#             if(filter_value == 'STORE_NAME'):
-#                 store = Stores.objects.filter(STORE_NAME__icontains=search_str).values('STORE_ID')
-#                 if(store):
-#                     all_stocks = Stocks.objects.filter(STORE_ID = store[0]['STORE_ID'])
-#             if(filter_value == 'PRODUCT_NAME'):
-#                 product = Products.objects.filter(PRODUCT_NAME__icontains=search_str).values('PRODUCT_ID')
-#                 if(product):
-#                     all_stocks = Stocks.objects.filter(PRODUCT_ID = product[0]['PRODUCT_ID'])    
-    
-#     for stock in all_stocks:
-#         x = Stores.objects.filter(STORE_ID=stock.STORE_ID).values('STORE_NAME')
-#         stock.STORE_ID = x[0]['STORE_NAME']
-#         y = Products.objects.filter(PRODUCT_ID=stock.PRODUCT_ID).values('PRODUCT_NAME')
-#         stock.PRODUCT_ID = y[0]['PRODUCT_NAME']
-        
-#     return render(request, 'stocks.html', {'stocks': all_stocks})
-
 def stocks(request):
     all_stocks = Stocks.objects.all()
     
@@ -139,39 +116,70 @@ def stocks(request):
                 if product:
                     all_stocks = all_stocks.filter(PRODUCT_ID=product[0]['PRODUCT_ID'])
 
-    # Fetch all store IDs and product IDs to minimize queries
     store_ids = set(stock.STORE_ID for stock in all_stocks)
     product_ids = set(stock.PRODUCT_ID for stock in all_stocks)
 
-    # Fetch store names and product names in bulk
     store_names = {store.STORE_ID: store.STORE_NAME for store in Stores.objects.filter(STORE_ID__in=store_ids)}
     product_names = {product.PRODUCT_ID: product.PRODUCT_NAME for product in Products.objects.filter(PRODUCT_ID__in=product_ids)}
 
-    # Update stock objects with corresponding names
     for stock in all_stocks:
         stock.STORE_ID = store_names.get(stock.STORE_ID, '')
         stock.PRODUCT_ID = product_names.get(stock.PRODUCT_ID, '')
 
     return render(request, 'stocks.html', {'stocks': all_stocks})
 
-
 def orders(request):
     all_orders = Orders.objects.all()
     
+
     if request.method == 'POST':
         search_str = request.POST.get('search')
         filter_value = request.POST.get('filter')
         if filter_value != '' and search_str != '':
-            if filter_value == 'STORE_NAME':
+            if filter_value == 'ORDER_ID':
+                all_orders = all_orders.filter(ORDER_ID=search_str)
+            elif filter_value == 'STORE_NAME':
                 store = Stores.objects.filter(STORE_NAME__icontains=search_str).values('STORE_ID')
                 if store:
                     all_orders = all_orders.filter(STORE_ID=store[0]['STORE_ID'])
             elif filter_value == 'CUSTOMER_NAME':
-                cuustomer = Customers.objects.filter(CUSTOMER_NAME__icontains=search_str).values('CUSTOMER_ID')
-                if cuustomer:
-                    all_orders = all_orders.filter(CUSTOMER_ID=cuustomer[0]['CUSTOMER_ID'])
+                customer = Customers.objects.filter(Q(FIRST_NAME__icontains=search_str) | Q(LAST_NAME__icontains=search_str)).values('CUSTOMER_ID')
+                if customer:
+                    all_orders = all_orders.filter(CUSTOMER_id=customer[0]['CUSTOMER_ID'])
+    
+    paginator = Paginator(all_orders, 10)  # Show 10 orders per page
 
+    page = request.GET.get('page')
+    try:
+        all_orders = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver the first page.
+        all_orders = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver the last page of results.
+        all_orders = paginator.page(paginator.num_pages)
+    
     return render(request, 'orders.html', {'orders': all_orders})
+
+# def orders(request):
+#     all_orders = Orders.objects.all()
+#     print("here")
+#     if request.method == 'POST':
+#         search_str = request.POST.get('search')
+#         filter_value = request.POST.get('filter')
+#         if filter_value != '' and search_str != '':
+#             if filter_value == 'ORDER_ID':
+#                 all_orders = all_orders.filter(ORDER_ID=search_str)
+#             elif filter_value == 'STORE_NAME':
+#                 store = Stores.objects.filter(STORE_NAME__icontains=search_str).values('STORE_ID')
+#                 if store:
+#                     all_orders = all_orders.filter(STORE_ID=store[0]['STORE_ID'])
+#             elif filter_value == 'CUSTOMER_NAME':
+#                 cuustomer = Customers.objects.filter(CUSTOMER_NAME__icontains=search_str).values('CUSTOMER_ID')
+#                 if cuustomer:
+#                     all_orders = all_orders.filter(CUSTOMER_ID=cuustomer[0]['CUSTOMER_ID'])
+
+#     return render(request, 'orders.html', {'orders': all_orders})
 
 def aboutus(request):
     return render(request, 'aboutus.html')
